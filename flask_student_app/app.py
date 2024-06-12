303adff313adaf984d1d10fa54b28cf40e4f4fe9
sev7nse7en7 <sev7nse7en@gmail.com>
2024-06-12 08:07:31 -0600

    added flask_student_app with student registration

---
+++ flask_student_app/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    grade = db.Column(db.String(10), nullable=False)
+
+@app.route('/')
+def index():
+    students = Student.query.all()
+    return render_template('index.html', students=students)
+
+@app.route('/add', methods=['POST'])
+def add_student():
+    name = request.form['name']
+    age = request.form['age']
+    grade = request.form['grade']
+
+    new_student = Student(name=name, age=age, grade=grade)
+    db.session.add(new_student)
+    db.session.commit()
+
+    return redirect('/')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
